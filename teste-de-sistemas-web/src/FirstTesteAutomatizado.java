6d308e2c6852b076ff33ace1f7ad334b55802640
Wesley Pereira de Jesus <unai.wesley@gmail.com>
2016-11-15 13:20:37 -0200

    Reverter
    Teste

--- teste-de-sistemas-web/src/FirstTesteAutomatizado.java
+++ teste-de-sistemas-web/src/FirstTesteAutomatizado.java
@@ -2,23 +2,16 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 
 public class FirstTesteAutomatizado {
 	
 	public static void main(String[] args) {
 		
-		
-//		System.setProperty("webdriver.gecko.driver", "C:\\Drivers Selenium\\geckodriver.exe");
-		System.setProperty("webdriver.chrome.driver", "C:\\Drivers Selenium\\chromedriver.exe");
-		
-//		DesiredCapabilities dc = DesiredCapabilities.firefox();
-//		dc.setCapability("marionette", true);
-//		
-//		WebDriver driver = new FirefoxDriver(dc);
-		WebDriver driver = new ChromeDriver();
+		WebDriver driver;
+//		System.setProperty("webdriver.gecko.driver", "C:\\Users\\wesleyjesus\\Desktop\\Suíte de Teste Documento Eletrônico\\geckodriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\wesleyjesus\\Desktop\\Suíte de Teste Documento Eletrônico\\chromedriver.exe");
+		driver =new ChromeDriver();
 		
 		driver.get("http://www.bing.com");
 		
